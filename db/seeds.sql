7bcca8f424485ca646ecc77b16f6cec09665dd10
shijir0927 <shijiree_mng@yahoo.com>
2019-11-05 14:40:05 -0800

    database schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 --Seeds file to populate database with some starter data--
 
-INSERT INTO burgers (burger_name, devoured) VALUES ("Awesome Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("First Burger", true);
 
-INSERT INTO burgers (burger_name, devoured) VALUES ("Good Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Second", false);
 
-INSERT INTO burgers (burger_name, devoured) VALUES ("Mondo Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Third", true);
