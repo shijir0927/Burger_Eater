ad4a0a66820a2f01336edec8ebf413fd18c2790e
shijir0927 <shijiree_mng@yahoo.com>
2019-11-05 14:19:49 -0800

    commit

---
+++ controllers/burgersController.js
@@ -0,0 +1,62 @@
+let express = require("express");
+let router = express.Router();
+let burger = require("../models/burger.js");
+
+//GET route to get burgers from database.
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    console.log("controller call")
+    let hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+//POST route to create/add a burger.
+router.post("/api/burgers", function(req, res) {
+  burger.create([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+//PUT route to update burger devoured state.
+router.put("/api/burgers/:id", function(req, res) {
+  let condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+//DELETE route to throw away a burger.
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+
+// Export routes for server.js to use.
+module.exports = router;
+
